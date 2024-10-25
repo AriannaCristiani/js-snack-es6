040fc631dff82a31c965097dab44c89388e9df02
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-10-25 15:54:03 +0200

    creazione array di oggetti

--- app.js
+++ app.js
@@ -3,6 +3,31 @@ Snack 1
 Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore.
+*/
+
+const bikes = [
+    {
+
+     modello: 'triban'
+     peso: '1830g'
+
+    },
+    {
+
+     modello: 'scott'
+     peso: '1750g'
+
+    },
+    {
+
+     modello: 'mountain'
+     peso: '2010g'
+
+    },
+
+];
+
+console.log()
 
 
 
@@ -14,15 +39,14 @@ Stampare a schermo la bici con peso minore.
 
 
 
-
-
-
+/*
 Snack2
 Creare un array di oggetti di squadre di calcio.
  Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+*/
 
 
 
@@ -36,8 +60,7 @@ Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subit
 
 
 
-
-
+/*
 Snack 3 (Bonus)
 Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
